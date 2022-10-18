6e6bb0676b870e2a95c14be8367d455a28f51853
johnworker <78348941+johnworker@users.noreply.github.com>
2022-10-18 08:04:43 +0800

    練習添加

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-$(document).ready(function () {
-    $('.title').hide();
-    // $("選擇器").事件();
-});
+// $(document).ready(function () {
+//     $('.title').hide();
+//     // $("選擇器").事件();
+// });
 
+$(document).ready(function () {
+    $('h1').click(function (event) { 
+        $('p').toggleClass('orange');
+        $('div').slideUp(1000).slideDown(2000);
+    });
+});
